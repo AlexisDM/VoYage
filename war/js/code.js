614df77b58677c07f73d873fbafc8311801d436a
Antoine Faure-audebert <a.faure-audebert@tpopt09.cpe.fr>
2013-11-18 17:29:17 +0100

    Fin Edit User

--- war/js/code.js
+++ war/js/code.js
@@ -141,15 +141,17 @@ function funcLoadUsers() {
 				  var cell6=row.insertCell(5);
 				  var cell7=row.insertCell(6);
 				  var cell8=row.insertCell(7);
+				  var cell9=row.insertCell(8);
 				  
 				  cell1.innerHTML = data[x].login;
 				  cell2.innerHTML = data[x].nom;
 				  cell3.innerHTML = data[x].prenom;
 				  cell4.innerHTML = data[x].age;
-				  cell5.innerHTML = data[x].creationAccount;
-				  cell6.innerHTML = data[x].lastConnexionDate;
-				  cell7.innerHTML = data[x].lastConnexionTime;
-				  cell8.innerHTML = "<button id=\"btnEdit\" onclick=\"funcEditUser('".concat(data[x].id).concat("');\">Edit</button>").concat("<button id=\"btnDelete\" onclick=\"funcDeleteUser('").concat(data[x].id).concat("');\">Delete</button>");
+				  cell5.innerHTML = data[x].email;
+				  cell6.innerHTML = data[x].creationAccount;
+				  cell7.innerHTML = data[x].lastConnexionDate;
+				  cell8.innerHTML = data[x].lastConnexionTime;
+				  cell9.innerHTML = "<button id=\"btnEdit\" onclick=\"funcEditUser('".concat(data[x].id).concat("');\">Edit</button>").concat("<button id=\"btnDelete\" onclick=\"funcDeleteUser('").concat(data[x].id).concat("');\">Delete</button>");
 				  }
 				
 				 var row=table.insertRow(0);
@@ -161,17 +163,40 @@ function funcLoadUsers() {
 				 var cell6=row.insertCell(5);
 				 var cell7=row.insertCell(6);
 				 var cell8=row.insertCell(7);
+				 var cell9=row.insertCell(8);
 				  
 				 cell1.innerHTML = "<b>Login</b>";
 				 cell2.innerHTML = "<b>Last Name</b>";
 				 cell3.innerHTML = "<b>First Name</b>";
 				 cell4.innerHTML = "<b>Age</b>";
-				 cell5.innerHTML = "<b>Creation Date</b>";
-				 cell6.innerHTML = "<b>Last Connexion Date</b>";
-				 cell7.innerHTML = "<b>Last Connexion Time</b>";
-				 cell8.innerHTML = "<b>Action</b>";
+				 cell5.innerHTML = "<b>E-mail</b>";
+				 cell6.innerHTML = "<b>Creation Date</b>";
+				 cell7.innerHTML = "<b>Last Connexion Date</b>";
+				 cell8.innerHTML = "<b>Last Connexion Time</b>";
+				 cell9.innerHTML = "<b>Action</b>";
 
 
+			}
+		});  
+}
+
+
+function funcUpdateUser() {
+	$.post("ManageUsers",
+		{
+			cmd:"UpdateUser",
+			id:$("#usr_id").text(),
+			nom:$("#usr_nom").val(),
+  		    prenom: $("#usr_prenom").val(),
+  		    age: $("#usr_age").val(),
+  			email: $("#usr_email").val(),
+  		    password: $("#usr_password").val()
+		},
+		function(data,status){
+			if(data == "Failed") {
+				alert("Error updating user")
+			} else {
+				window.location.href = 'manageusers.html?login='.concat(data.login).concat("&nom=").concat(data.nom).concat("&prenom=").concat(data.prenom).concat("&lastConnexionDate=").concat(data.lastConnexionDate).concat("&lastConnexionTime=").concat(data.lastConnexionTime);
 			}
 		});  
 }
@@ -186,9 +211,7 @@ function funcEditUser(param) {
 			if(data == "Failed") {
 				alert("Error accessing other page")
 			} else {
-				var tab = new Array();
-				tab = data.split(";");
-				window.location.href = 'manageusers.html?login='.concat(tab[0]).concat("&nom=").concat(tab[1]).concat("&prenom=").concat(tab[2]).concat("&lastConnexionDate=").concat(tab[3]).concat("&lastConnexionTime=").concat(tab[4]);
+				window.location.href = 'edituser.html?login='.concat(data.login).concat("&nom=").concat(data.nom).concat("&prenom=").concat(data.prenom).concat("&age=").concat(data.age).concat("&lastConnexionDate=").concat(data.lastConnexionDate).concat("&lastConnexionTime=").concat(data.lastConnexionTime).concat("&creationAccount=").concat(data.creationAccount).concat("&email=").concat(data.email).concat("&id=").concat(data.id);
 			}
 		});  
 }
@@ -215,4 +238,17 @@ function funcFillDataAdmin() {
 	$("#fieldAdminLogin").text(getURLParameter("login"));
 	$("#fieldAdminLastConnexionDate").text(getURLParameter("lastConnexionDate"));
 	$("#fieldAdminLastConnexionTime").text(getURLParameter("lastConnexionTime"));
+}
+
+function funcUserLoad() {
+	$("#usr_login").text(getURLParameter("login"));
+	$("#usr_prenom").val(getURLParameter("prenom"));
+	$("#usr_nom").val(getURLParameter("nom"));
+	$("#usr_age").val(getURLParameter("age"));
+	$("#usr_email").val(getURLParameter("email"));
+	$("#usr_creation").text(getURLParameter("creationAccount"));
+	$("#usr_lastConnexionDate").text(getURLParameter("lastConnexionDate"));
+	$("#usr_lastConnexionTime").text(getURLParameter("lastConnexionTime"));
+	$("#usr_id").text(getURLParameter("id"));
+	
 }
